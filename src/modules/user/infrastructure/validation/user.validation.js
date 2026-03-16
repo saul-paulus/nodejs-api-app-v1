7f4bbb19495279489fdcbacd482376d3a2b99770
saul-paulus <saulpaulus17@icloud.com>
2026-03-16 23:24:39 +0700

    Fix: perbaiki validasi user

--- src/modules/user/infrastructure/validation/user.validation.js
+++ src/modules/user/infrastructure/validation/user.validation.js
@@ -5,13 +5,13 @@ const userRegistrasiValidation = Joi.object({
   id_personal: Joi.string().max(255).required(),
   password: Joi.string().max(255).required(),
   codeuker: Joi.string().max(255).required(),
-  id_wewenang: Joi.number().integer().required(),
+  id_role: Joi.number().integer().required(),
 });
 
 const userUpdateValidation = Joi.object({
   username: Joi.string().max(255).required(),
   codeuker: Joi.string().max(255).required(),
-  id_wewenang: Joi.number().integer().required(),
+  id_role: Joi.number().integer().required(),
 });
 
 export { userRegistrasiValidation, userUpdateValidation };
