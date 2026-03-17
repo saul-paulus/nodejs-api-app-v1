96fce3478df74b0cd0b9ee1dfcf9e01494fc8844
saul-paulus <saulpaulus17@icloud.com>
2026-03-17 13:26:08 +0700

    Fix: perbaiki user login

--- src/modules/auth/interfaces/controllers/AuthController.js
+++ src/modules/auth/interfaces/controllers/AuthController.js
@@ -8,8 +8,8 @@ export default class AuthController {
 
   async handlerLoginUser(req, res, next) {
     try {
-      const result = await this.loginUser.execute(req.body);
-      res.status(StatusCodes.OK).json(successResponse(StatusCodes.OK, 'User has been authenticated', result));
+      await this.loginUser.execute(req.body);
+      res.status(StatusCodes.OK).json(successResponse(StatusCodes.OK, 'User has been authenticated', '-'));
     } catch (error) {
       next(error);
     }
